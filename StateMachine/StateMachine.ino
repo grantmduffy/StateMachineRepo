1eeb17193639a0731ac89ca5b8125212d39daca2
grantmduffy <grantmduffy@gmail.com>
2016-10-16 13:38:36 -0700

    Barebones
    Strip actual state value/ functionality code

--- StateMachine/StateMachine.ino
+++ StateMachine/StateMachine.ino
@@ -1,32 +1,15 @@
 
-//State Types
-typedef enum {
-  
-} ;
-
-//States
-bool ledOn;
-
 void setup() {
   Serial.begin(9600);
   OCR0A = 0xAF;
   TIMSK0 |= _BV(OCIE0A);
-
-  //Initial States
-  ledOn = false;
 }
 
 void loop() { //Constantly try to read commands from the serial
   while (!Serial.available()){}
   char cmd = Serial.read();
   switch (cmd){
-    case ';':
-      break;
-    case 'P':
-      runPrint();
-      break;
-    case 'L':
-      runSetLed();
+    default:
       break;
   }
 }
@@ -43,19 +26,3 @@ void runPrint(){
     Serial.print(ch);
   }
 }
-
-void runSetLed(){
-  while (!Serial.available()){}
-  char ch = Serial.read();
-  switch (ch){
-    case ';':
-      break;
-    case '0':
-      ledOn = false;
-      break;
-    case '1':
-      ledOn = true;
-      break;
-  }
-}
-
